f22fcebb7ef7a7e0121ade3ec9c2e2de73d628f5
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2023-02-11 23:51:18 +0800

    Setup

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,33 +1,42 @@
 ﻿using BepInEx;
 using System.Security.Permissions;
+using System;
 
 // Allows access to private members
 #pragma warning disable CS0618
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618
 
-namespace TestMod;
-
-[BepInPlugin("com.author.testmod", "Test Mod", "0.1.0")]
-sealed class Plugin : BaseUnityPlugin
-{
-    bool init;
-
-    public void OnEnable()
-    {
-        // Add hooks here
-        On.RainWorld.OnModsInit += Init;
-    }
-
-    private void Init(On.RainWorld.orig_OnModsInit orig, RainWorld self)
-    {
-        orig(self);
-
-        if (!init) {
-            init = true;
-
-            // Initialize assets, your mod config, and anything that uses RainWorld here
-            Logger.LogDebug("Hello world!");
-        }
-    }
+namespace RENAME_ME {
+	
+	[BepInPlugin("frostbird347.renameme", "REMAME_ME", "0.1.0")]
+	public sealed class Plugin : BaseUnityPlugin {
+	{
+		bool init;
+		private PluginOptions Options = null;
+		bool configWorking = false;
+		
+		public void OnEnable()
+		{
+			// Add hooks here
+			On.RainWorld.OnModsInit += Init;
+		}
+		
+		private void Init(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+		{
+			orig(self);
+			
+			if (!init) {
+				init = true;
+				
+				try {
+					Options = new PluginOptions(this, Logger);
+					MachineConnector.SetRegisteredOI("frostbird347.hairlesssaint", Options);
+					configWorking = true;
+				} catch (Exception err) {
+					Logger.LogError(err);
+					configWorking = false;
+				}
+			}
+		}
 }
